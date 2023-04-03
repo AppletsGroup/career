650e7a37652ca793aecbd6388e1e5d225da62a28
stevenzg <guoyinfeng@live.com>
2023-04-03 20:54:09 +0800

    fix: config applet-shell TailwindCSS

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,8 @@
 module.exports = {
   content: [
     './index.html',
-    './src/**/*.{js,ts,jsx,tsx}'
+    './src/**/*.{js,ts,jsx,tsx}',
+    './node_modules/applet-shell/dist/*.{js,jsx,ts,tsx,mjs}'
   ],
   theme: {
     extend: {}
